0b56924d6bd29a4f03733b4a410a9e5882cf5aef
sh20raj <shaswatraj3@gmail.com>
2024-03-24 14:46:15 +0530

    encoder decoder working condition

---
+++ script.js
@@ -0,0 +1,20 @@
+
+document.getElementById('conversionForm').addEventListener('submit', function(event) {
+  event.preventDefault();
+  
+  let urlInput = document.getElementById('urlInput').value;
+  let binary = textToBinary(urlInput);
+  let ilFormat = binaryToIl(binary);
+  document.getElementById('ilOutput').textContent = ilFormat;
+});
+
+document.getElementById('decodeForm').addEventListener('submit', function(event) {
+  event.preventDefault();
+  
+  let ilInput = document.getElementById('ilInput').value;
+  let binary = IlTobinary(ilInput);
+  let originalText = binaryToText(binary);
+  document.getElementById('decodedOutput').textContent = originalText;
+});
+
+
